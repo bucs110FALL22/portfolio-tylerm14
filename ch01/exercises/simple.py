bfaa71bdf6c367852b2f6f3f92ba9219ca67c750
Tyler Moy <112582406+tylerm14@users.noreply.github.com>
2022-09-01 21:17:58 +0000

    Your Environment Lab Completed

---
+++ ch01/exercises/simple.py
@@ -0,0 +1,16 @@
+# Part 1
+print(10 * 5)
+print(10 ** 2)
+print(15 / 10)
+print(15 // 10)
+print(-15 // 10)
+print(15 % 10)
+print(10 % 15)
+print(10 % 10)
+print(0 % 10)
+print(10 / 15)
+  # this last answer     repeats infinitely
+
+# Part 2
+rate = input('Input Current Euro -> Dollar Exchange Rate: ')
+amount = raw_input('')
--- ch01/lab/main.py
+++ ch01/lab/main.py
@@ -1,1 +1,1 @@
-
+print("Hello Tyler")
