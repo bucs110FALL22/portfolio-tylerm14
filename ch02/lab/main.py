301bf85b7f3e91547f260981dfaeb554294ef002
Tyler Moy <112582406+tylerm14@users.noreply.github.com>
2022-09-06 21:15:56 +0000

    Simple Data Lab Completed

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,4 +1,4 @@
-
+import random
 #Part A
 weeks = 16
 classes = 5
@@ -6,5 +6,11 @@ tuition = 6000
 cost_per_week = ((tuition / classes) / weeks)
 print("Cost per week:", cost_per_week)
 
-
+classes_per_week = 3
+cost_per_class = (cost_per_week / classes_per_week)
+print("Cost per class:", cost_per_class, type(cost_per_class))
 #Part B
+devicesList = ["Phone", "PC", "Tablet", "Laptop", "TV"]
+
+randDevice = random.choice(devicesList)
+print("Random Device Chosen:", randDevice)
